0e3325f118ca1f812ed4d3e4c9ef47fac882c0c9
retrorocket <1858830+retrorocket@users.noreply.github.com>
2023-06-05 15:35:10 +0900

    timezone設定を外に出す

--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -1,6 +1,7 @@
+import { settings } from "@config/config.json";
 import { DateTime } from "luxon";
 const dateFormat = (datetime: Date, format?: string) => {
-  const dt = DateTime.fromJSDate(datetime, { zone: "Asia/Tokyo" });
+  const dt = DateTime.fromJSDate(datetime, { zone: settings.timezone });
 
   if (format) return dt?.toFormat(format);
   return dt?.toISO();
