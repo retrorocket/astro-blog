b9e675d2c020728a9c4b750721c1456ae8cb5be2
retrorocket <1858830+retrorocket@users.noreply.github.com>
2023-08-22 19:00:58 +0900

    sm環境でボタンから文字列がはみ出すのを修正する

--- src/layouts/shortcodes/Button.tsx
+++ src/layouts/shortcodes/Button.tsx
@@ -18,7 +18,7 @@ const Button = ({
       rel={`noopener noreferrer ${
         rel ? (rel === "follow" ? "" : rel) : "nofollow"
       }`}
-      className={`btn mb-4 me-4 ${
+      className={`btn mb-4 me-4 h-auto ${
         style === "outline" ? "btn-outline-primary" : "btn-primary"
       } border-primary hover:text-white hover:no-underline`}
     >
